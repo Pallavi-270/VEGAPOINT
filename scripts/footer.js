775c52e35ed880623e7d46dacbac760be9301c0c
pallavi-270 <veeramanikantapallavi@gmail.com>
2025-02-13 22:17:16 +0530

    all files

---
+++ scripts/footer.js
@@ -0,0 +1,32 @@
+const footer=`<div id="footer">
+        <div id="one">
+            <h4>Vegapoint</h4>
+            <p>About Us</p>
+            <p>Green VegaPoint</p>
+            <p>PrivacyPolicy</p>
+            <p>Terms and Conditions</p>
+            <p>VP Blog</p>
+        </div>
+        <div id="two">
+            <h4>Help</h4>
+            <p>FAQs</p>
+            <p>Contact Us</p>
+        </div>
+        <div id="three">
+            <img id="icon" class="aa" src="./images/icon.png" width="100px" />
+            <img id="amazon"
+                src="https://masai-course.s3.ap-south-1.amazonaws.com/editor/uploads/2022-12-21/Screenshot%202022-12-21%20at%202.47.09%20PM_558991.png"
+                width="100px" />
+            <img id="appleplay"
+                src="https://masai-course.s3.ap-south-1.amazonaws.com/editor/uploads/2022-12-21/Screenshot%202022-12-21%20at%202.47.43%20PM_652243.png"
+                width="100px" />
+        </div>
+        <div id="four">
+            <h4>Cities we survive</h4>
+            <p>Bangalore|Chennai|EastGodavari|Madras|Pune|Vizag|WestGodavari|</p>
+        </div>
+        <div id="five">
+            <p>@copy Supermarket Grocery Pvt Ltd</p>
+        </div>
+    </div>`;
+    document.getElementById("footer").innerHTML=footer;
---
+++ scripts/navbar.js
@@ -0,0 +1,11 @@
+const navbar=`  <div id="header">
+        <img id="icon" src="./images/icon.png" width="100px" />
+        <input id="search" type="text" placeholder="Search for Products">
+        <nav id="nav">
+            <a href="index.html">Home</a>
+            <a href="products.html">Products</a>
+            <a href="cart.html">🛒cart</a>
+            <a href="login.html">Login/signup</a>
+        </nav>
+    </div>`;
+    document.getElementById("nav").innerHTML=navbar
